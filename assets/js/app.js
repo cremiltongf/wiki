54f14a11fddc6d17d858613f2db912c77eeda65f
Cremilton Fernandes <cremilton@outlook.com>
2023-12-08 00:15:29 -0300

    Correção de JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,20 @@
-window.addEventListener('load', function(){
-    //sumario mobile
-    var toogleMenu = document.querySelectorAll('.toggle-menu');
-    var menuContainer    = document.querySelector('.menu-mobile');
+window.addEventListener("load", menuSidebar);
 
-    for (var i = 0; i < toogleMenu.length; i++){
-        toogleMenu[i].addEventListener('click', menuAction);
-    }
-
-    document.addEventListener('keyup', function(e){
-        if(e.key == 27){
-            if(menuContainer.classList.contains('show-menu')){
-                menuAction();
-            }
-        }
-    });
-
-    function menuAction() {
-        if(menuContainer.classList.contains('show-menu')){
-            menuContainer.classList.remove('show-menu');
-        }
-        else {
-            menuContainer.classList.add('show-menu');
-        }
-    }
-
-    //button top
-    window.onscroll = function() { scrollFunction() };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-            var element = document.getElementById("btn-topo");
-            element.classList.add("btn-topo-show");
-        } else {
-            var element = document.getElementById("btn-topo");
-            element.classList.remove("btn-topo-show");
-        }
-    }
-});
+//button top
+// function buttonTop(){
+//     window.onscroll = function() {
+//         if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+//             var element = document.getElementById("btn-topo");
+//             element.classList.add("btn-topo-show");
+//         } else {
+//             var element = document.getElementById("btn-topo");
+//             element.classList.remove("btn-topo-show");
+//         }
+//     }
+// }
 
 // show and hidden sidebar menu in mobile
-window.addEventListener("load", menuSidebar);
-function menuSidebar(){
+function menuSidebar() {
     let hiddenOverflow = document.querySelector("body");
     let togglerSidebar = document.querySelectorAll(".sidebar-toggler");
     let containerSidebar = document.querySelector(".sidebar-container");
