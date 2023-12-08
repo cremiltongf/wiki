ec09472f475f1ea90a45522132c1417d1d6f6b9a
Cremilton Fernandes <cremilton@outlook.com>
2023-12-08 12:15:01 -0300

    Ativando funções nas páginas corretas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,31 @@
 //button top
 function returnTop(){
     let buttonTop = document.querySelector(".return-top");
-    if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
-        buttonTop.classList.add("show-arrow");
-    } else {
-        buttonTop.classList.remove("show-arrow");
+    if (buttonTop !== null){
+        if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
+            buttonTop.classList.add("show-arrow");
+        } else {
+            buttonTop.classList.remove("show-arrow");
+        }
     }
 }
 
 // show and hidden nav-sidebar, show and hidden scroll
 function menuSidebar(){
-    let hiddenOverflow = document.querySelector("body");
-    let togglerSidebar = document.querySelectorAll(".sidebar-toggler");
-    let containerSidebar = document.querySelector(".sidebar-container");
-
-    for (var i = 0; i < togglerSidebar.length; i++){
-        togglerSidebar[i].addEventListener('click', actionSidebar);
-    }
-
-    function actionSidebar(){
-        containerSidebar.classList.toggle("sidebar-show");
-        hiddenOverflow.classList.toggle("hidden-scroll");
+    let navbarTrue = document.querySelector(".navbar-true");
+    if(navbarTrue !== null){
+        let hiddenOverflow = document.querySelector("body");
+        let togglerSidebar = document.querySelectorAll(".sidebar-toggler");
+        let containerSidebar = document.querySelector(".sidebar-container");
+    
+        for (var i = 0; i < togglerSidebar.length; i++){
+            togglerSidebar[i].addEventListener('click', actionSidebar);
+        }
+        console.log("Não estou na home");
+        function actionSidebar(){
+            containerSidebar.classList.toggle("sidebar-show");
+            hiddenOverflow.classList.toggle("hidden-scroll");
+        }
     }
 }
 
