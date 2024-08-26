e198487188f2a3367c1a63d84b16cb57bec87a9b
Cremilton Fernandes <cremilton@outlook.com>
2024-08-26 11:15:28 -0300

    Refactoring js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,34 @@
-//return top
-function returnTop(){
-    let buttonTop = document.querySelector(".return-top");
-    if (buttonTop !== null){
-        if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
-            buttonTop.classList.add("show-arrow");
-        } else {
-            buttonTop.classList.remove("show-arrow");
-        }
-    }
-}
-
-// nav-sidebar e hidden-scroll
-function menuSidebar(){
-    let navbarTrue = document.querySelector(".navbar-true");
-    if(navbarTrue !== null){
-        let hiddenOverflow = document.querySelector("body");
-        let togglerSidebar = document.querySelectorAll(".sidebar-toggler");
-        let containerSidebar = document.querySelector(".sidebar-container");
+(function( win, doc ){
     
-        for (var i = 0; i < togglerSidebar.length; i++){
-            togglerSidebar[i].addEventListener('click', actionSidebar);
-        }
-        function actionSidebar(){
-            containerSidebar.classList.toggle("sidebar-show");
-            hiddenOverflow.classList.toggle("hidden-scroll");
+    'use strict';
+    
+    function returnTopSite(){
+        let $buttonTop = doc.querySelector( '[data-js="top-site"]' );
+        if ( $buttonTop !== null ){
+            if ( doc.body.scrollTop > 640 || doc.documentElement.scrollTop > 640 )
+                $buttonTop.classList.add( 'show-arrow' );
+            else
+                $buttonTop.classList.remove( 'show-arrow' );
         }
     }
-}
+    win.addEventListener( 'scroll', returnTopSite, false );
 
-window.addEventListener("load", menuSidebar);
-window.addEventListener("scroll", returnTop);
+    function openCloneSidebarMenu(){
+        let $navbarTrue = doc.querySelector( '[data-js="navbar-true"]' );
+        let $hiddenOverflow = doc.querySelector( '[data-js="overflowOnOff"]' );
+        let $containerSidebar = doc.querySelector( '[data-js="sidebar-container"]' );
+        let $togglerSidebar = doc.querySelectorAll( '[data-toggler="sidebar-toggler"]' );
+
+        if( $navbarTrue !== null ){
+            for ( let i = 0; i < $togglerSidebar.length; i++ ){
+                $togglerSidebar[i].addEventListener( 'click', actionSidebar, false );
+            }
+            function actionSidebar(){
+                $hiddenOverflow.classList.toggle( 'hidden-scroll' );
+                $containerSidebar.classList.toggle( 'sidebar-show' );
+            }
+        }
+    }
+    win.addEventListener( 'load', openCloneSidebarMenu, false );
+
+})( window, document );
